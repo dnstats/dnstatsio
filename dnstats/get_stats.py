797ff0463d8aaff76377e596162a64c523a4c781
Matthew Burket <m@tthewburket.com>
2019-11-21 21:45:38 -0600

    Started on SPF

--- dnstats/get_stats.py
+++ dnstats/get_stats.py
@@ -1,14 +1,15 @@
-import dns
+import dns.resolver
 from time import sleep
 import csv
+import re
 
 
 def get_dns():
-    with open('dnstats/sites_rank.csv', 'r') as sites_file:
+    with open('dnstats/sites_rank2.csv', 'r') as sites_file:
 
-        with open('dnsstats/dns_results.csv', 'w') as results_files:
+        with open('dnstats/dns_results.csv', 'w') as results_files:
             csv_reader = csv.DictReader(sites_file)
-            fieldnames = ['rank', 'site', 'mail', 'txt', 'caa', 'dmarc']
+            fieldnames = ['rank', 'site', 'mail', 'txt', 'caa', 'dmarc', 'has_spf']
             results_writer = csv.DictWriter(results_files, fieldnames)
             results_writer.writeheader()
 
@@ -18,8 +19,9 @@ def get_dns():
                 txt = safe_query(site, 'txt')
                 caa = safe_query(site, 'caa')
                 dmarc = safe_query('_dmarc.' + site, 'txt')
+                is_spf = has_spf(txt)
                 results_writer.writerow({'rank': line['rank'], 'site': site, 'mail': mail, 'txt': txt,
-                                         'caa': caa, 'dmarc': dmarc})
+                                         'caa': caa, 'dmarc': dmarc, 'has_spf': is_spf})
                 sleep(2)
                 break
 
@@ -40,6 +42,18 @@ def safe_query(site: str, type: str):
     else:
         return None
 
+
+def has_spf(ans):
+    for r in ans:
+        if r.startswith('"v=spf'):
+            return True
+    return False
+
+
+def spf_final_qualifier(record: str):
+    pattern = re.compile(r"/[+?~-]all/")
+    q = pattern.match(record)
+    return q
 
 
 if __name__ == '__main__':
