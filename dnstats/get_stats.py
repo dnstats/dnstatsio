62f179ae35d2aea8d41d74b15cf30edc327bf979
Matthew Burket <m@tthewburket.com>
2019-11-22 23:06:38 -0600

    Started celery verison

--- dnstats/get_stats.py
+++ dnstats/get_stats.py
@@ -1,7 +1,6 @@
-import dns.resolver
 from time import sleep
 import csv
-import re
+from dnstats.dnsutils import safe_query, spf_final_qualifier, has_spf
 
 
 def get_dns():
@@ -9,52 +8,32 @@ def get_dns():
 
         with open('dnstats/dns_results.csv', 'w') as results_files:
             csv_reader = csv.DictReader(sites_file)
-            fieldnames = ['rank', 'site', 'mail', 'txt', 'caa', 'dmarc', 'has_spf']
+            fieldnames = ['rank', 'site', 'mail', 'txt', 'caa', 'dmarc', 'has_spf', 'spf_qualifier', 'has_dnssec']
             results_writer = csv.DictWriter(results_files, fieldnames)
             results_writer.writeheader()
 
             for line in csv_reader:
                 site = line['site']
-                mail = safe_query(site, 'mx')
-                txt = safe_query(site, 'txt')
-                caa = safe_query(site, 'caa')
-                dmarc = safe_query('_dmarc.' + site, 'txt')
-                is_spf = has_spf(txt)
+                caa, dmarc, is_spf, mail, spf_qualifier, txt, has_dnsec = get_stats(site)
                 results_writer.writerow({'rank': line['rank'], 'site': site, 'mail': mail, 'txt': txt,
-                                         'caa': caa, 'dmarc': dmarc, 'has_spf': is_spf})
+                                         'caa': caa, 'dmarc': dmarc, 'has_spf': is_spf, 'spf_qualifier': spf_qualifier,
+                                         'has_dnssec': has_dnsec})
                 sleep(2)
                 break
 
 
-def safe_query(site: str, type: str):
-    r = None
-    try:
-        r = dns.resolver.query(site, type)
-    except dns.resolver.NoAnswer:
-        pass
-    except dns.resolver.NXDOMAIN:
-        pass
-    if r:
-        results = list()
-        for ans in r:
-            results.append(ans.to_text())
-        return results
-    else:
-        return None
-
-
-def has_spf(ans):
-    for r in ans:
-        if r.startswith('"v=spf'):
-            return True
-    return False
-
-
-def spf_final_qualifier(record: str):
-    pattern = re.compile(r"/[+?~-]all/")
-    q = pattern.match(record)
-    return q
-
+def get_stats(site):
+    mail = safe_query(site, 'mx')
+    txt = safe_query(site, 'txt')
+    caa = safe_query(site, 'caa')
+    dmarc = safe_query('_dmarc.' + site, 'txt')
+    is_spf, spf_record = has_spf(txt)
+    spf_qualifier = ""
+    ds = safe_query(site, 'ds')
+    has_dnssec = ds != ""
+    if is_spf:
+        spf_qualifier = spf_final_qualifier(spf_record)
+    return caa, dmarc, is_spf, mail, spf_qualifier, txt, has_dnssec
 
 if __name__ == '__main__':
-    get_dns()
+    get_stats('assignitapp.com')
