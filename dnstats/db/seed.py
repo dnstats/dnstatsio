a2156c3b00e8ec96bce625d00f376a3a2f1df259
Matthew Burket <m@tthewburket.com>
2019-11-24 19:12:55 -0600

    Started database seed

---
+++ dnstats/db/seed.py
@@ -0,0 +1,13 @@
+import csv
+
+import dnstats.db.models as models
+from dnstats.db import db_session
+
+
+def seed_db(filename: str) -> None:
+    with open(filename, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            site = models.Sites(current_rank=row['GlobalRank'], domain=row['domain'])
+            db_session.add(site)
+            db_session.commit()
