4fe30e75252d4c78836ebfa93f3a00fef299168b
Matthew Burket <m@tthewburket.com>
2019-11-24 22:55:05 -0600

    Fix details about database connecting

--- dnstats/db/__init__.py
+++ dnstats/db/__init__.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 
 
-engine = create_engine('postgres://mburket@/dnstats', pool_recycle=3600, pool_size=100)
+engine = create_engine('postgres://mburket@localhost/dnstats', pool_recycle=3600, pool_size=100)
 
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
