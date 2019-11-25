381370d0f7776af07190abd5d1592d5c2cd885b2
Matthew Burket <m@tthewburket.com>
2019-11-24 22:55:33 -0600

    Use the right csv columns for seeding the database

--- dnstats/db/seed.py
+++ dnstats/db/seed.py
@@ -8,6 +8,6 @@ def seed_db(filename: str) -> None:
     with open(filename, 'r') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
-            site = models.Sites(current_rank=row['GlobalRank'], domain=row['domain'])
+            site = models.Sites(current_rank=row['rank'], domain=row['site'])
             db_session.add(site)
             db_session.commit()
