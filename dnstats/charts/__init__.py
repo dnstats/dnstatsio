4f7661dffe2a52c7079d7ce78a9b52e2e02b345c
Matthew Burket <m@tthewburket.com>
2019-12-05 18:15:35 -0600

    Create a html report from a run

--- dnstats/charts/__init__.py
+++ dnstats/charts/__init__.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import datetime
 from jinja2 import Environment, FileSystemLoader
 
 from dnstats.db import db_session, engine
@@ -19,7 +20,7 @@ def render_pie(categories, filename: str):
                     '{}.png'.format(filename)])
 
 
-def get_categories_from_query(run_id: int, query: str):
+def get_categories_from_query(run_id: int, query: str) -> [()]:
     # This method assumes that the count is in column 0, name of the group is in column 1, and color is in column 2
     categories = []
     with engine.connect() as connection:
@@ -31,3 +32,67 @@ def get_categories_from_query(run_id: int, query: str):
             categories.append(category)
     return categories
 
+
+def get_categories_from_adoption_query(run_id: int, query: str) -> [()]:
+    categories = []
+    with engine.connect() as connection:
+        total_count = db_session.query(models.SiteRun).filter_by(run_id=run_id).count()
+        result_set = connection.execute(query)
+        for row in result_set:
+            yes = round((row[0]/total_count), 1) * 100
+            yes_count = row[0]
+        no = round(((total_count - yes_count)/total_count), 1) * 100
+        categories.append({'percent': yes, 'name': 'Yes', 'color': 'green'})
+        categories.append({'percent': no, 'name': 'No', 'color': 'red'})
+        return categories
+
+
+def run_report(query: str, report: str, adoption: bool, run_id: int):
+    if adoption:
+        categories = get_categories_from_adoption_query(run_id, query)
+    else:
+        categories = get_categories_from_query(run_id, query)
+    filename = _create_timedate_filename(report)
+    render_pie(categories, filename)
+    return filename, report
+
+
+def _create_timedate_filename(basefilename: str) -> str:
+    now = datetime.datetime.now()
+    return '{}_{}'.format(now.strftime('%Y-%m-%d_%H_%M_%S'), basefilename)
+
+
+def create_reports(run_id: int):
+    spf_adoption_query = "select count(*) from site_runs sr where sr.run_id = {} and sr.has_spf is true".format(run_id)
+
+    spf_reports_query = "select count(*), sp.display_name, sp.color from site_runs sr join spf_policy sp on sr.spf_policy_id = sp.id " \
+                  "where sr.run_id = {} group by sp.display_name, sp.color".format(run_id)
+
+    dmarc_adoption_query = "select count(*) from site_runs where run_id = {} and has_dmarc is true".format(run_id)
+
+    dmarc_policy_query = "select count(*), dp.display_name, dp.color from site_runs sr " \
+                         "join dmarc_policy dp on sr.dmarc_policy_id = dp.id where sr.run_id = {} " \
+                         "group by dp.display_name, dp.color, dp.display_name".format(run_id)
+
+    caa_adoption_query = "select count(*) from site_runs where run_id = {} and has_caa is true".format(run_id)
+
+    caa_reporting = "select count(*) from site_runs where run_id = {} and has_caa_reporting is true".format(run_id)
+
+    filenames = [run_report(spf_adoption_query, 'SPF Adoption', True, run_id),
+                 run_report(spf_reports_query, 'SPF Policy', False, run_id),
+                 run_report(dmarc_adoption_query, 'DMARC Adaption', True, run_id),
+                 run_report(dmarc_policy_query, 'DMARC Policy', False, run_id),
+                 run_report(caa_adoption_query, 'CAA Adoption', True, run_id),
+                 run_report(caa_reporting, 'CAA Reporting', True, run_id)]
+
+    create_html(filenames)
+
+
+def create_html(filemaes: [()]):
+    file_loader = FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates'))
+    env = Environment(loader=file_loader)
+    template = env.get_template('index.html.j2')
+    result = template.render(charts=filemaes)
+    filename = _create_timedate_filename('index') + '.html'
+    with open(filename, 'w') as file:
+        file.write(result)
