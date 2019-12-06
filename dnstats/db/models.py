98a987d2cca85b1051ca923af0da30306c778dc1
Matthew Burket <m@tthewburket.com>
2019-12-05 20:05:42 -0600

    Added constraints and improved seeding

--- dnstats/db/models.py
+++ dnstats/db/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Boolean, String, DateTime, ForeignKey, BigInteger, Text
+from sqlalchemy import Column, Boolean, String, DateTime, ForeignKey, BigInteger, Text, UniqueConstraint
 
 Base = declarative_base()
 
@@ -7,51 +7,54 @@ Base = declarative_base()
 class DmarcPolicy(Base):
     __tablename__ = 'dmarc_policy'
     id = Column(BigInteger, primary_key=True)
-    policy_string = Column(String)
-    display_name = Column(String)
-    color = Column(String)
+    policy_string = Column(String, nullable=False)
+    display_name = Column(String, nullable=False)
+    color = Column(String, nullable=False)
+    UniqueConstraint('policy_string')
 
 
 class Run(Base):
     __tablename__ = 'runs'
     id = Column(BigInteger, primary_key=True)
-    start_time = Column(DateTime)
-    start_rank = Column(BigInteger)
-    end_rank = Column(BigInteger)
+    start_time = Column(DateTime, nullable=False)
+    start_rank = Column(BigInteger, nullable=False)
+    end_rank = Column(BigInteger, nullable=False)
 
 
 class Site(Base):
     __tablename__ = 'sites'
     id = Column(BigInteger, primary_key=True)
-    domain = Column(String)
-    current_rank = Column(BigInteger)
+    domain = Column(String, nullable=False)
+    current_rank = Column(BigInteger, nullable=False)
+    UniqueConstraint('domain')
 
 
 class SiteRun(Base):
     __tablename__ = 'site_runs'
     id = Column(BigInteger, primary_key=True)
-    site_id = Column(BigInteger, ForeignKey('sites.id'))
-    run_id = Column(BigInteger, ForeignKey('runs.id'))
-    run_rank = Column(BigInteger)
+    site_id = Column(BigInteger, ForeignKey('sites.id'), nullable=False)
+    run_id = Column(BigInteger, ForeignKey('runs.id'), nullable=False)
+    run_rank = Column(BigInteger, nullable=False)
     caa_record = Column(Text)
-    has_caa = Column(Boolean)
-    has_caa_reporting = Column(Boolean)
+    has_caa = Column(Boolean, nullable=False)
+    has_caa_reporting = Column(Boolean, nullable=False)
     caa_issue_count = Column(BigInteger)
     caa_wildcard_count = Column(BigInteger)
-    has_dmarc = Column(Boolean)
-    dmarc_policy = Column(Boolean)
-    sub_dmarc_policy = Column(Boolean)
-    has_dmarc_aggregate_reporting = Column(Boolean)
-    has_dmarc_forensic_reporting = Column(Boolean)
+    has_dmarc = Column(Boolean, nullable=False)
+    dmarc_policy = Column(Boolean, nullable=False)
+    sub_dmarc_policy = Column(Boolean, nullable=False)
+    has_dmarc_aggregate_reporting = Column(Boolean, nullable=False)
+    has_dmarc_forensic_reporting = Column(Boolean, nullable=False)
     dmarc_policy_id = Column(BigInteger, ForeignKey('dmarc_policy.id'))
     dmarc_sub_policy_id = Column(BigInteger, ForeignKey('dmarc_policy.id'))
     dmarc_record = Column(Text)
-    has_spf = Column(Boolean)
-    spf_policy_id = Column(BigInteger, ForeignKey('spf_policy.id'))
+    has_spf = Column(Boolean, nullable=False)
+    spf_policy_id = Column(BigInteger, ForeignKey('spf_policy.id'), nullable=False)
     txt_records = Column(Text)
     ds_records = Column(Text)
     mx_records = Column(Text)
     ns_records = Column(Text)
+    UniqueConstraint('site_id', 'run_id')
 
     def has_dmarc_reporting(self):
         return self.has_dmarc_aggregate_reporting or self.has_dmarc_forensic_reporting
@@ -60,9 +63,14 @@ class SiteRun(Base):
 class SpfPolicy(Base):
     __tablename__ = 'spf_policy'
     id = Column(BigInteger, primary_key=True)
-    qualifier = Column(String)
-    display_name = Column(String)
-    color = Column(String)
+    qualifier = Column(String, nullable=False)
+    display_name = Column(String, nullable=False)
+    color = Column(String, nullable=False)
+    UniqueConstraint('policy_string')
 
 
-
+class Color(Base):
+    __timename__ = 'color'
+    id = Column(BigInteger, primary_key=True)
+    name = Column(String, nullable=True)
+    UniqueConstraint('name')
--- dnstats/db/seed.py
+++ dnstats/db/seed.py
@@ -5,9 +5,44 @@ from dnstats.db import db_session
 
 
 def seed_db(filename: str) -> None:
+    _seed_dmarc_policy()
+    _seed_sites(filename)
+    _seed_spf()
+
+
+def _seed_spf():
+    spf_policies = [
+        ('+all', 'Pass', 'magenta'),
+        ('?all', 'Neutral', 'orange'),
+        ('~all', 'Soft-fail', 'green'),
+        ('-all', 'Fail', 'blue'),
+        ('no_policy', 'No Policy', 'red')
+    ]
+
+    for spf_pol in spf_policies:
+        spf_policy = models.SpfPolicy(qualifier=spf_pol[0], display_name=spf_pol[1], color=spf_pol[2])
+        db_session.add(spf_policy)
+        db_session.commit()
+
+
+def _seed_sites(filename):
     with open(filename, 'r') as file:
         csv_reader = csv.DictReader(file)
+
         for row in csv_reader:
-            site = models.Sites(current_rank=row['rank'], domain=row['site'])
+            site = models.Site(current_rank=row['rank'], domain=row['site'])
             db_session.add(site)
             db_session.commit()
+
+
+def _seed_dmarc_policy():
+    dmarc_policies = [
+        ('none', 'None', 'orange'),
+        ('quarantine', 'Quarantine', 'green'),
+        ('reject', 'Reject', 'blue'),
+        ('no_policy', 'No Policy', 'red')
+    ]
+    for dmarc_pol in dmarc_policies:
+        dmarc_policy = models.DmarcPolicy(policy_string=dmarc_pol[0], display_name=dmarc_pol[1], color=dmarc_pol[2])
+        db_session.add(dmarc_policy)
+        db_session.commit()
