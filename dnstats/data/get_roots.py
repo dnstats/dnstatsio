2c39292d20d04632d5a7292e0608dc7245e3cec1
Matthew Burket <m@tthewburket.com>
2019-12-14 13:09:59 -0600

    Change files for imports

--- dnstats/data/get_roots.py
+++ dnstats/data/get_roots.py
@@ -4,12 +4,12 @@ import csv
 
 def parse():
     sites = dict()
-    with open('data.csv', 'r') as csv_file:
+    with open('majestic_million.csv', 'r') as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
             sites.setdefault(publicsuffix2.get_sld((row['IDN_Domain'])), row['GlobalRank'])
 
-    with open('sites_rank2.csv', 'w') as csv_file:
+    with open('sites_rank.csv', 'w') as csv_file:
         fieldnames = ['site', 'rank']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
