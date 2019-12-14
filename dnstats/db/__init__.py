bc94b983212ca998afe69a2d1dcf583006028898
Matthew Burket <m@tthewburket.com>
2019-12-14 13:09:32 -0600

    Auto commit and create db

--- dnstats/db/__init__.py
+++ dnstats/db/__init__.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
+from sqlalchemy_utils import database_exists, create_database
 
 
-engine = create_engine('postgres://mburket@localhost/dnstats', pool_recycle=3600, pool_size=100)
+engine = create_engine('postgres://mburket@localhost/dnstats', pool_recycle=3600, pool_size=250,
+                       isolation_level='AUTOCOMMIT')
 
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+if not database_exists(engine.url):
+    create_database(engine.url)
