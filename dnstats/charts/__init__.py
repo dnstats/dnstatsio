435615bee72a951447754c2e771d33a3b34f90a1
Matthew Burket <m@tthewburket.com>
2019-12-21 16:42:16 -0600

    PEP8 on charts and testing out d3 pie charts

--- dnstats/charts/__init__.py
+++ dnstats/charts/__init__.py
@@ -1,5 +1,4 @@
 import os
-import time
 import subprocess
 import datetime
 from jinja2 import Environment, FileSystemLoader
@@ -30,7 +29,7 @@ def get_categories_from_query(run_id: int, query: str) -> [()]:
         result_set = connection.execute(query)
 
         for row in result_set:
-            category = {'percent': round((row[0]/total_count) * 100, 3), 'name': row[1], 'color': row[2]}
+            category = {'percent': round((row[0] / total_count) * 100, 3), 'name': row[1], 'color': row[2]}
             categories.append(category)
     return categories
 
@@ -41,9 +40,9 @@ def get_categories_from_adoption_query(run_id: int, query: str) -> [()]:
         total_count = db_session.query(models.SiteRun).filter_by(run_id=run_id).count()
         result_set = connection.execute(query)
         for row in result_set:
-            yes = round((row[0]/total_count), 3) * 100
+            yes = round((row[0] / total_count), 3) * 100
             yes_count = row[0]
-        no = round(((total_count - yes_count)/total_count), 3) * 100
+        no = round(((total_count - yes_count) / total_count), 3) * 100
         categories.append({'percent': yes, 'name': 'Yes', 'color': 'green'})
         categories.append({'percent': no, 'name': 'No', 'color': 'red'})
         return categories
@@ -68,7 +67,7 @@ def create_reports(run_id: int):
     spf_adoption_query = "select count(*) from site_runs sr where sr.run_id = {} and sr.has_spf is true".format(run_id)
 
     spf_reports_query = "select count(*), sp.display_name, sp.color from site_runs sr join spf_policy sp on sr.spf_policy_id = sp.id " \
-                  "where sr.run_id = {} group by sp.display_name, sp.color".format(run_id)
+                        "where sr.run_id = {} group by sp.display_name, sp.color".format(run_id)
 
     dmarc_adoption_query = "select count(*) from site_runs where run_id = {} and has_dmarc is true".format(run_id)
 
@@ -76,16 +75,28 @@ def create_reports(run_id: int):
                          "join dmarc_policy dp on sr.dmarc_policy_id = dp.id where sr.run_id = {} " \
                          "group by dp.display_name, dp.color, dp.display_name".format(run_id)
 
-    caa_adoption_query = "select count(*) from site_runs where run_id = {} and has_caa is true".format(run_id)
+    caa_adoption_query = 'select count(*) from site_runs where run_id = {} and has_caa is true'.format(run_id)
 
     caa_reporting = "select count(*) from site_runs where run_id = {} and has_caa_reporting is true".format(run_id)
+
+    mx_query = 'select count(*) from site_runs where run_id = {} and mx_records is not null'.format(run_id)
+
+    dmarc_sub_policy_adoption = 'select count(*) from site_runs sr where run_id = {} ' \
+                                'and (dmarc_sub_policy_id != 4 or dmarc_sub_policy_id = 5)'.format(run_id)
+
+    dmarc_subpolicy_query = "select count(*), dp.display_name, dp.color from site_runs sr " \
+                            "join dmarc_policy dp on sr.dmarc_sub_policy_id = dp.id where sr.run_id = {} " \
+                            "group by dp.display_name, dp.color, dp.display_name".format(run_id)
 
     filenames = [run_report(spf_adoption_query, 'SPF Adoption', True, run_id),
                  run_report(spf_reports_query, 'SPF Policy', False, run_id),
                  run_report(dmarc_adoption_query, 'DMARC Adaption', True, run_id),
+                 run_report(dmarc_sub_policy_adoption, 'DMARC Subdomain Policy Adaption', True, run_id),
                  run_report(dmarc_policy_query, 'DMARC Policy', False, run_id),
+                 run_report(dmarc_subpolicy_query, 'DMARC Subdomain Policy', False, run_id),
+                 run_report(mx_query, 'Has MX Records', True, run_id),
                  run_report(caa_adoption_query, 'CAA Adoption', True, run_id),
-                 run_report(caa_reporting, 'CAA Reporting', True, run_id)]
+                 run_report(caa_reporting, 'CAA Reporting', True, run_id), :wq]
 
     create_html(filenames, run_id)
 
