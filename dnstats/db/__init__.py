361129a11944fbed2eef9b213262f67644bd6e59
Matthew Burket <m@tthewburket.com>
2020-01-28 20:20:01 -0600

    Database config is now in ENV vars

--- dnstats/db/__init__.py
+++ dnstats/db/__init__.py
@@ -1,10 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 from sqlalchemy_utils import database_exists, create_database
 
 
-engine = create_engine('postgres://mburket@localhost/dnstats', pool_recycle=3600, pool_size=250,
+engine = create_engine(os.environ.get('DB'), pool_recycle=3600, pool_size=250,
                        isolation_level='AUTOCOMMIT')
 
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
