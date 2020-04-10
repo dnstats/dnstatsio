ef7c2167d7bdd6ba8d0463cf02c42ec8d99d3328
Matthew Burket <m@tthewburket.com>
2020-04-09 21:04:55 -0500

    Give credit where credit is due
    Shamles stole some code

--- dnstats/utils/__init__.py
+++ dnstats/utils/__init__.py
@@ -1,4 +1,8 @@
+"""
+Source: https://chrisalbon.com/python/data_wrangling/break_list_into_chunks_of_equal_size/
+"""
+
+
 def chunks(array, size):
     for i in range(0, len(array), size):
         yield array[i:i + size]
-
