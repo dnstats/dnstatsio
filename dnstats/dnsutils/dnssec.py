db5ffaebc34f86ffd4338b71c453c65e3372618f
Matthew Burket <m@tthewburket.com>
2020-06-17 19:35:34 -0500

    DNSSEC scaning works now
    -1 instaid of None

--- dnstats/dnsutils/dnssec.py
+++ dnstats/dnsutils/dnssec.py
@@ -1,21 +1,14 @@
 def parse_ds(ans: list) -> [int, int]:
     if ans:
-        if len(ans) != 1:
-            return None, None
-        parts = ans[0].split(' ')
-        if len(parts) != 4:
-            return None, None
+        parts = str(ans[0]).split(' ')
         algorithm = parts[1]
         digest_type = parts[2]
         return algorithm, digest_type
-    return None, None
+    return -1, -1
 
 
 def parse_dnskey(ans: list):
     if ans:
-        if len(ans) != 1:
-            return None
         parts = ans[0].split(' ')
-        if len(parts) != 4:
-            return None
         return parts[2]
+    return -1
