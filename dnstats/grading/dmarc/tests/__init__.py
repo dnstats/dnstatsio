e38ae92fd1061011aebe6d11766ac0cd6f845a1b
Matthew Burket <m@tthewburket.com>
2020-07-19 09:38:20 -0500

    Added more tests for DMARC grading

--- dnstats/grading/dmarc/tests/__init__.py
+++ dnstats/grading/dmarc/tests/__init__.py
@@ -12,6 +12,11 @@ class TestDmarc(unittest.TestCase):
         grade = grade_dmarc('v=DMARC1; p=reject;', 'example.com')
         self.assertEqual(grade, 80)
 
+
+    def test_bad_policy(self):
+        grade = grade_dmarc('v=DMARC1; p=tacoman;', 'example.com')
+        self.assertEqual(grade, 0)
+
     def test_quan(self):
         grade = grade_dmarc('v=DMARC1; p=quarantine;', 'example.com')
         self.assertEqual(grade, 45)
@@ -20,7 +25,18 @@ class TestDmarc(unittest.TestCase):
         grade = grade_dmarc('v=DMARC1; p=reject; pct=100', 'example.com')
         self.assertEqual(grade, 80)
 
+    def test_pct_reject_0(self):
+        grade = grade_dmarc('v=DMARC1; p=reject; pct=0', 'example.com')
+        self.assertEqual(grade, 73)
+
     def test_no_policy(self):
         grade = grade_dmarc('v=DMARC1;', 'example.com')
         self.assertEqual(grade, 0)
 
+    def test_reject_with_rua(self):
+        grade = grade_dmarc('v=DMARC1; p=reject; rua=mailto:postmaster@dnstats.io', 'google.com')
+        self.assertEqual(grade, 85)
+
+    def test_perfect_score(self):
+        grade = grade_dmarc('v=DMARC1; adkim=s; aspf=s; fo=1; p=reject; rua=mailto:postmaster@dnstats.io; ruf=mailto:postmaster@dnstats.io', 'google.com')
+        self.assertEqual(grade, 104)
