6f7dcdc86ed4ff473a00a9fae77f08cf03ec28de
Matthew Burket <m@tthewburket.com>
2020-09-13 12:15:26 -0500

    Added CAA tests and fixed bugs in CAA dnsvalidate

--- dnstats/dnsvalidate/caa.py
+++ dnstats/dnsvalidate/caa.py
@@ -18,6 +18,32 @@ class CAAErrors(enum.Enum):
     IODEF_INVALID_URL = 9
     ISSUEWILD_DOMAIN_INVALID = 10
     ISSUE_DOMAIN_INVALID = 11
+    TAG_TOO_LONG = 12
+
+
+class CAA:
+    """
+    DNS validation for CAA
+    """
+    def __init__(self, caa_records: list, domain: str):
+        self.caa_records = caa_records
+        self.domain = domain
+
+    @property
+    def iodef(self) -> list:
+        return validate(self.caa_records, self.domain)['iodef']
+
+    @property
+    def issue(self) -> list:
+        return validate(self.caa_records, self.domain)['issue']
+
+    @property
+    def issuewild(self) -> list:
+        return validate(self.caa_records, self.domain)['issuewild']
+
+    @property
+    def errors(self) -> list:
+        return validate(self.caa_records, self.domain)['errors']
 
 
 def validate(caa_result_set: list, domain: str) -> dict:
@@ -31,6 +57,7 @@ def validate(caa_result_set: list, domain: str) -> dict:
     errors = list()
     issue = list()
     issuewild = list()
+    iodef = list()
     if len(caa_result_set) == 0:
         errors.append(CAAErrors.NO_CAA_RECORDS)
         return {'errors': errors}
@@ -48,58 +75,75 @@ def validate(caa_result_set: list, domain: str) -> dict:
         except ValueError:
             errors.append(CAAErrors.INVALID_FLAG)
             continue
+        if flag > 128:
+            errors.append(CAAErrors.INVALID_FLAG)
         tag_re = re.compile('^[a-z0-9]+$')
         if len(tag_re.findall(tag)) != 1:
             errors.append(CAAErrors.INVALID_TAG)
+        if len(tag) > 15:
+            errors.append(CAAErrors.TAG_TOO_LONG)
 
         quote_re = re.compile('"')
         value_quote_count = len(quote_re.findall(value))
-        if value_quote_count != 0 or value_quote_count != 2:
+        if value_quote_count != 0 and value_quote_count != 2:
             errors.append(CAAErrors.VALUE_QUOTE_ERROR)
+            continue
 
-        if not value.starts_with('"') and value.__contains__(' '):
+        if not value.startswith('"') and value.__contains__(' '):
             errors.append(CAAErrors.VALUE_NOT_QUOTED)
-
+            continue
         # Section 5.2
         if tag == 'issue':
             value = value.replace('"', '')
             issue.append(value)
             if value == ';':
                 issuewild.append(value)
-            domain = value.split(';')
-            if not validate_fqdn(domain):
+            c_domain = value.split(';')
+            if not validate_fqdn(c_domain[0]):
                 errors.append(CAAErrors.ISSUE_DOMAIN_INVALID)
-                break
+                continue
             issuewild.append(value)
         # Section 5.3
         elif tag == 'issuewild':
             value = value.replace('"', '')
             if value == ';':
                 issuewild.append(value)
-            domain = value.split(';')
-            if not validate_fqdn(domain):
+            c_domain = value.split(';')
+            if not validate_fqdn(c_domain[0]):
                 errors.append(CAAErrors.ISSUEWILD_DOMAIN_INVALID)
-                break
+                continue
             issuewild.append(value)
 
         elif tag == 'iodef':
             value = value.replace('"', '')
             if value == ';':
+                iodef.append(';')
                 continue
             iodef_schemes = ['http', 'https', 'mailto']
             has_scheme = False
             for scheme in iodef_schemes:
-                if value.starts_with(scheme):
+                if value.startswith(scheme):
                     has_scheme = True
             if not has_scheme:
                 errors.append(CAAErrors.IODEF_NO_SCHEME)
-            if value.starts_with('mailto'):
-                email = value.remove('mailto:', '')
+                continue
+            iodef.append(value)
+            if value.startswith('mailto'):
+                email = value.replace('mailto:', '')
                 if not validate_email(email):
                     errors.append(CAAErrors.IODEF_INVALID_EMAIL)
+                    continue
 
-            elif value.starts_with('http') or value.starts_with('https'):
+            elif value.startswith('http') or value.startswith('https'):
                 if not validate_url(value):
                     errors.append(CAAErrors.IODEF_INVALID_URL)
+                    continue
+
+    result = dict()
+    result['errors'] = errors
+    result['issue'] = issue
+    result['issuewild'] = issuewild
+    result['iodef'] = iodef
+    return result
 
 
--- dnstats/dnsvalidate/tests/test_caa.py
+++ dnstats/dnsvalidate/tests/test_caa.py
@@ -1,9 +1,90 @@
 import unittest
 
 from dnstats.dnsvalidate.caa import validate as validate_caa
+from dnstats.dnsvalidate.caa import CAAErrors
 
 
 class TestCaa(unittest.TestCase):
-    def test_something(self):
-        grade = validate_caa(list(), 'example.com')
-        self.assertEqual(0, grade)
+    def test_no_caa(self):
+        output = validate_caa(list(), 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.NO_CAA_RECORDS])
+
+    def test_invalid_record(self):
+        output = validate_caa(['taco'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.INVALID_PROPERTY_STRUCTURE])
+
+    def test_invalid_record2(self):
+        output = validate_caa(['taco taco taco'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.INVALID_FLAG])
+
+    def test_invalid_flag(self):
+        output = validate_caa(['1000 taco taco'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.INVALID_FLAG])
+
+    def test_too_long_tag(self):
+        output = validate_caa(['0 tacotacotacotaco taco'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.TAG_TOO_LONG])
+
+    def test_too_many_quotes(self):
+        output = validate_caa(['0 issue ""taco"'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.VALUE_QUOTE_ERROR])
+
+    def test_too_few_quotes(self):
+        output = validate_caa(['0 issue "taco'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.VALUE_QUOTE_ERROR])
+
+    def test_not_quoted(self):
+        output = validate_caa(['0 issue taco taco'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.VALUE_NOT_QUOTED])
+
+    def test_issue(self):
+        output = validate_caa(['0 issue dnstats.io'], 'example.com')
+        self.assertEqual(output['errors'], [])
+
+    def test_wildissue_quoted(self):
+        output = validate_caa(['0 issuewild "dnstats.io"'], 'example.com')
+        self.assertEqual(output['errors'], [])
+
+    def test_wildissue_quoted_semi(self):
+        output = validate_caa(['0 issuewild "dnstats.io; value=taco"'], 'example.com')
+        self.assertEqual(output['errors'], [])
+
+    def test_issue_quoted_semi(self):
+        output = validate_caa(['0 issue "dnstats.io; value=taco"'], 'example.com')
+        self.assertEqual(output['errors'], [])
+
+    def test_wildissue(self):
+        output = validate_caa(['0 issuewild dnstats.io'], 'example.com')
+        self.assertEqual(output['errors'], [])
+
+    def test_issue_badhost(self):
+        output = validate_caa(['0 issue dnstats,io'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.ISSUE_DOMAIN_INVALID])
+
+    def test_wildissue_badhost(self):
+        output = validate_caa(['0 issuewild dnstats,io'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.ISSUEWILD_DOMAIN_INVALID])
+
+    def test_iodef_mail(self):
+        output = validate_caa(['0 iodef mailto:caa@dnstats.io'], 'example.com')
+        self.assertEqual(output['errors'], [])
+
+    def test_iodef_web(self):
+        output = validate_caa(['0 iodef https://dnstats.io/caa'], 'example.com')
+        self.assertEqual(output['errors'], [])
+
+    def test_iodef_mail_invalid(self):
+        output = validate_caa(['0 iodef mailto:caa@dnstats,io'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.IODEF_INVALID_EMAIL])
+
+    def test_iodef_web_invalid(self):
+        output = validate_caa(['0 iodef https://dnstats,io/caa'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.IODEF_INVALID_URL])
+
+    def test_iodef_no_mailto(self):
+        output = validate_caa(['0 iodef caa@dnstats.io'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.IODEF_NO_SCHEME])
+
+    def test_iodef_bad_scheme(self):
+        output = validate_caa(['0 iodef taco://dnstats.io/caa'], 'example.com')
+        self.assertEqual(output['errors'], [CAAErrors.IODEF_NO_SCHEME])
