a513d2dadfaea97a9c4e6051c13c564bd56e5d1e
Matthew Burket <m@tthewburket.com>
2020-10-21 22:30:48 -0500

    Fix seed on inital seed of remarks
    It will fail since None does not have an id.
    
    Signed-off-by: Matthew Burket <m@tthewburket.com>

--- dnstats/db/seed.py
+++ dnstats/db/seed.py
@@ -90,7 +90,7 @@ def _seed_remark_arrays(remark_type_db_spf: models.RemarkType, spf: list) -> Non
         remark_db = db_session.query(models.Remark).filter_by(remark_type_id=remark_type_db_spf.id,
                                                               enum_value=remark[2]).scalar()
 
-        if remark_db.id:
+        if remark_db:
             remark_db.remark_level = remark[0]
             remark_db.name = remark[1]
             remark_db.enum_value = remark[2]
