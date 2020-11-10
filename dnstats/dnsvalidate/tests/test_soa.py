516ad1c4ab43ae78c6e7ec226d61b3fadef11aa4
Matthew Burket <m@tthewburket.com>
2020-11-09 18:18:50 -0600

    Add more SOA tests
    Signed-off-by: Matthew Burket <m@tthewburket.com>

--- dnstats/dnsvalidate/tests/test_soa.py
+++ dnstats/dnsvalidate/tests/test_soa.py
@@ -15,3 +15,14 @@ class TestNs(unittest.TestCase):
         result = Soa(['ns1.dnsimple.com. admin.dnsimple.com. taco 86400 7200 604800 300'], 'dnstats.io')
         self.assertNotEqual(0, len(result.errors))
 
+    def test_soa_missing_thing(self):
+        result = Soa(['ns1.dnsimple.com. admin.dnsimple.com. taco 86400 7200 604800'], 'dnstats.io')
+        self.assertNotEqual(0, len(result.errors))
+
+    def test_soa_missing_things(self):
+        result = Soa(['ns1.dnsimple.com. admin.dnsimple.com. taco 86400 7200'], 'dnstats.io')
+        self.assertNotEqual(0, len(result.errors))
+
+    def test_no_soa(self):
+        result = Soa(['ns1.dnsimple.com. admin.dnsimple.com. taco 86400 7200'], 'dnstats.io')
+        self.assertNotEqual(0, len(result.errors))
