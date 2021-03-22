dnstats/dnstatsio

commit eb8529feaa013e438dc3ccc7feb175ca72893248

Make DNS validate handle blank DMARC records
This was needed BIMI checking

dnstats/db/seed.py
@@ -42,7 +42,8 @@ def _seed_remarks():
         (1, 'Invalid Failure reporting (rf) Value', 7),
         (1, 'Invalid Aggregate Reporting interval (ri) Value', 8),
         (1, 'Invalid Percent Value', 9),
-        (0, 'Invalid DMARC Record Start', 10)
+        (0, 'Invalid DMARC Record Start', 10),
+        (0, 'No DMARC Record')
     ]
 
     spf = [
@@ -161,7 +162,7 @@ def _seed_remark_arrays(remark_type_db_spf: models.RemarkType, spf: list) -> Non
 
 
 def _seed_remark_types():
-    remark_types = ['spf', 'dmarc', 'caa', 'ns', 'soa', 'mx']
+    remark_types = ['spf', 'dmarc', 'caa', 'ns', 'soa', 'mx', 'bimi']
 
     for remark_type in remark_types:
         remark_type_s = db_session.query(models.RemarkType).filter_by(name=remark_type).scalar()

dnstats/dnsvalidate/dmarc.py
@@ -15,6 +15,7 @@ class DmarcErrors(Enum):
     INVALID_RI_VALUE = 8
     INVALID_PCT_VALUE = 9
     INVALID_DMARC_RECORD_START = 10
+    NO_DMARC_RECORD = 11
 
 
 class Dmarc:
@@ -81,6 +82,11 @@ def validate(dmarc_result_set: list) -> dict:
     dmarc_record_values = dict()
     tag_count = dict()
     errors = list()
+    if len(dmarc_result_set) < 1:
+        errors.append(DmarcErrors.NO_DMARC_RECORD)
+        dmarc_record_values['errors'] = errors
+        return dmarc_record_values
+
     if len(dmarc_result_set) > 1:
         errors.append(DmarcErrors.MULTIPLE_DMARC_RECORDS)
         dmarc_record_values['errors'] = errors
@@ -152,7 +158,7 @@ def validate(dmarc_result_set: list) -> dict:
                 dmarc_record_values['sp'] = value
                 if value not in ['none', 'quarantine', 'reject']:
                     errors.append(DmarcErrors.INVALID_SUBDOMAIN_POLICY)
-            elif tag is 'v':
+            elif tag == 'v':
                 dmarc_record_values['v'] = value
             else:
                 dmarc_record_values[tag] = value

dnstats/dnsvalidate/tests/test_dmarc.py
@@ -5,8 +5,13 @@ from dnstats.dnsvalidate.dmarc import Dmarc, DmarcErrors
 
 class TestDmarc(unittest.TestCase):
     def test_ramdom_text(self):
-        d = list()
-        d.append('taco')
+        d = ['taco']
         dmarc_record = Dmarc(d)
         self.assertFalse(dmarc_record.is_valid)
         self.assertTrue(dmarc_record.errors.__contains__(DmarcErrors.INVALID_DMARC_RECORD_START))
+
+    def test_blank(self):
+        d = list()
+        dmarc_record = Dmarc(d)
+        self.assertFalse(dmarc_record.is_valid)
+        self.assertTrue(dmarc_record.errors.__contains__(DmarcErrors.NO_DMARC_RECORD))

commit 514d0c9368a8cf4c759fb65f090a5dd2a4f241b1

Fix bug in seed that was introduced in eb8529feaa013e438dc3ccc7feb175ca72893248

dnstats/db/seed.py
@@ -43,7 +43,7 @@ def _seed_remarks():
         (1, 'Invalid Aggregate Reporting interval (ri) Value', 8),
         (1, 'Invalid Percent Value', 9),
         (0, 'Invalid DMARC Record Start', 10),
-        (0, 'No DMARC Record')
+        (0, 'No DMARC Record', 11),
     ]
 
     spf = [